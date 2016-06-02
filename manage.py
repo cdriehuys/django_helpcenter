a1a91fe82dd5515388b1c84e64cbe6a0458dfab8
Chathan Driehuys <cdriehuys@gmail.com>
2016-06-02 19:43:17 -0400

    Creating basic django app structure.

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+
+from django.core import management
+
+
+# Point to the correct settings for testing
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ test_settings.py
@@ -0,0 +1,72 @@
+# Dummy secret key for testing
+
+SECRET_KEY = 'psst'
+
+
+# Create an in memory sqlite database.
+# Since it's only used for testing, there's no need to save it to a
+# file.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
+ROOT_URLCONF = 'helpcenter.urls'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    # Custom apps
+    'helpcenter',
+)
+
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+)
+
+
+STATIC_ROOT = '/tmp/'
+STATIC_URL = '/static/'
