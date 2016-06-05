f80ac8e6eb80a1cc08d3258674e4520b5471ebd2
Chathan Driehuys <cdriehuys@gmail.com>
2016-06-05 12:19:19 -0400

    Automatically running coverage when testing.

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 from django.core import management
 
@@ -10,4 +11,17 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
 
 
 if __name__ == "__main__":
+    testing = 'test' in sys.argv
+
+    if testing:
+        from coverage import Coverage
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
     management.execute_from_command_line()
+
+    if testing:
+        cov.stop()
+        cov.save()
+        cov.report()
