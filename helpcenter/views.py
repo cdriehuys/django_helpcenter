2fcbb209374c9193a2c9f41e04ef3d70957ce556
Chathan Driehuys <cdriehuys@gmail.com>
2016-06-08 13:39:27 -0400

    Adding search view.

--- helpcenter/views.py
+++ helpcenter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 from helpcenter import models
+from helpcenter.backends.search import SimpleSearch
 
 
 class ArticleDetailView(generic.DetailView):
@@ -53,3 +54,36 @@ class IndexView(generic.View):
         context['categories'] = categories
 
         return context
+
+
+class SearchView(generic.View):
+    """ View for searching article database """
+    template_name = 'helpcenter/search.html'
+    query_parameter = 'q'
+    search_backend = SimpleSearch()
+
+    def get(self, request, *args, **kwargs):
+        """ Handle GET requests """
+        self.request = request
+
+        return render(request, self.template_name, self.get_context_data())
+
+    def get_context_data(self, *args, **kwargs):
+        """ Get context data for a request """
+        context = {}
+
+        query = self.request.GET.get(self.query_parameter, None)
+
+        articles = self.get_search_results(query)
+        context['articles'] = articles
+
+        context['query'] = query
+
+        return context
+
+    def get_search_results(self, query):
+        """ Return the results of a given query """
+        if query is None:
+            return models.Article.objects.none()
+
+        return self.search_backend.search(query)
